ae99a9ca195315d87960fd12fba8699a9631876d
linsong <stevengrove@users.noreply.huggingface.co>
2024-01-31 06:39:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 os.system("pip uninstall mmcv-full")
+os.system("pip install torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cu118")
 os.system("mim install 'mmengine>=0.6.0'")
 os.system("mim install 'mmcv>=2.0.0rc4,<2.1.0'")
 os.system("mim install 'mmdet>=3.0.0,<4.0.0'")
