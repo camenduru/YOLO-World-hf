ae93021b5d9227bf4fde78aac5a300b3fce3f257
linsong <stevengrove@users.noreply.huggingface.co>
2024-01-31 07:50:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 os.system("pip uninstall -y mmcv-full")
 os.system("mim install 'mmengine>=0.6.0'")
-os.system("mim install 'mmcv>=2.0.0rc4,<2.1.0'")
+os.system("pip install mmcv==2.0.1 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.0/index.html")
 os.system("mim install 'mmdet>=3.0.0,<4.0.0'")
 os.system("mim install 'mmyolo'")
 os.system("pip install -e .")
+os.system("python tools/demo.py configs/pretrain/yolo_world_l_t2i_bn_2e-4_100e_4x8gpus_obj365v1_goldg_train_lvis_minival.py yolow-v8_l_clipv2_frozen_t2iv2_bn_o365_goldg_pretrain.pth")
 
 import argparse
 import os.path as osp
